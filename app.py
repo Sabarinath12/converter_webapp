f2f4e9c55a5a57ca438e1576706adf06cd0337a9
Sabarinath <88625894+Sabarinath12@users.noreply.github.com>
2024-02-04 19:45:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,30 +19,29 @@ def convert():
     if not video:
         return 'Error: No video file received.'
 
-    # Set the paths for the video and converted audio files
+  
     video_path = os.path.join(app.config['UPLOAD_FOLDER'], video.filename)
 
-    # Generate a random string of lowercase letters and digits
+   
     random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
 
     audio_filename = os.path.splitext(video.filename)[0] + '_' + random_string + '.wav'
     audio_path = os.path.join(app.config['UPLOAD_FOLDER'], audio_filename)
 
-    # Save the video file
+   
     video.save(video_path)
 
-    # Convert video to WAV using FFmpeg
+   
     try:
         subprocess.run(['ffmpeg', '-i', video_path, '-acodec', 'pcm_s16le', '-ar', '44100', '-ac', '2', audio_path], check=True)
     except subprocess.CalledProcessError:
-        # Remove the uploaded video file
+       
         os.remove(video_path)
-        return redirect(url_for('index'))  # Redirect to the index page or display a success message
-
-    # Remove the uploaded video file
+        return redirect(url_for('index'))  
+    
     os.remove(video_path)
 
-    # Send the converted audio file as a download
+    
     return send_from_directory(app.config['UPLOAD_FOLDER'], audio_filename, as_attachment=True)
 
 if __name__ == '__main__':
