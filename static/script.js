3aba4659f7c59d325601db3140c8db085357d42a
sabarinath.js <88625894+Sabarinath12@users.noreply.github.com>
2024-08-22 19:57:43 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -3,33 +3,26 @@ var loadingBox = document.getElementById('loadingBox');
 var fileInput = document.getElementById('file');
 var dragArea = document.getElementById('dragArea');
 var downloadStatus = document.getElementById('downloadStatus');
-
 convertButton.addEventListener('click', convertAndDownload);
-
 function convertAndDownload() {
   var file = fileInput.files[0];
   if (!file) {
     alert('Please select a video file.');
     return;
   }
-
   var fileSize = file.size;
   if (fileSize > 100 * 1024 * 1024) {
     alert('File size exceeds the maximum limit of 100MB.');
     return;
   }
-
   convertButton.disabled = true;
   loadingBox.style.display = 'flex';
-
   var reader = new FileReader();
   reader.onload = function (event) {
     var videoData = event.target.result;
     var videoBlob = new Blob([videoData], { type: file.type });
-
     var formData = new FormData();
     formData.append('video', videoBlob, file.name);
-
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/convert', true);
     xhr.responseType = 'blob';
@@ -37,16 +30,13 @@ function convertAndDownload() {
       if (xhr.readyState === XMLHttpRequest.DONE) {
         if (xhr.status === 200) {
           var blob = xhr.response;
-
           var link = document.createElement('a');
           link.href = window.URL.createObjectURL(blob);
           link.download = file.name.replace(/\.[^/.]+$/, '') + '.wav';
           link.style.display = 'none';
-
           document.body.appendChild(link);
           link.click();
           document.body.removeChild(link);
-
           showStatusMessage('Thank you for downloading!', false);
         } else {
           showStatusMessage('Failed to convert video.', true);
@@ -59,7 +49,6 @@ function convertAndDownload() {
   };
   reader.readAsArrayBuffer(file);
 }
-
 function showStatusMessage(message, isError) {
   downloadStatus.textContent = message;
   if (isError) {
